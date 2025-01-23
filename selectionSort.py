78ad56b6862e7ce83e8c972d76c90fb8cc4e6306
gamscodes <gaminee.ram30@gmail.com>
2025-01-23 18:47:35 -0500

    Selection sort

---
+++ selectionSort.py
@@ -0,0 +1,14 @@
+def selectionSort(arr):
+    n = len(arr)
+
+    for i in range(n):
+        min_index = i
+        for j in range(i + 1, n):
+            if arr[j] < arr[min_index]:
+                min_index = j
+        arr[i], arr[min_index] = arr[min_index], arr[i]
+
+
+arr = [4, 1, 3, 9, 7]
+selectionSort(arr)
+print(arr)
