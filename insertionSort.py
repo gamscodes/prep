d3c8fbe9793277bb914e962fbbdbbfffeb1d8711
gamscodes <gaminee.ram30@gmail.com>
2025-01-23 19:52:41 -0500

    insertion sort

---
+++ insertionSort.py
@@ -0,0 +1,14 @@
+def insertionSort(arr):
+    n = len(arr)
+    for i in range(1, n):
+        key = arr[i]
+        j = i - 1
+        while j >= 0 and arr[j] > key:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
+
+
+arr = [4, 1, 3, 9, 7]
+insertionSort(arr)
+print(arr)
